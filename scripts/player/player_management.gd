3c3b0b83330b5c3b786c1e6ff7bd1a5c02d9a02a
OutOfNet <neteoris@proton.me>
2025-05-09 09:16:07 +0200

    Added horizontal camera control.
    Added a way to move the camera horizontally with arrows on pc and the right joystick on controller/console, with the camera moving 45 degrees smoothly each time a camera movement input is entered, however it won't rotate again if it isn't done rotating, this is intended to prevent issues due to the method used to rotate it, one of those including the camera moving infinitely very rapidly, which can cause epilepsy related seizures (the problem is already fixed, however if you encounter it again, please let me know as soon as possible by creating an issue on the repository).

--- scripts/player/player_management.gd
+++ scripts/player/player_management.gd
@@ -6,35 +6,72 @@ const JUMP_FORCE = 50
 
 export var speed = 15.0
 
+var target_rotation = 0
 var velocity = Vector3.ZERO
 var health = MAX_HEALTH
+var is_rotating = false
 
-func _ready():
-	pass # Replace with function body.
+
+
+func rotate_player(rotate_direction):
+	if rotate_direction == "left" && is_rotating == false:
+		if is_equal_approx(3.141593, rotation.y) == true:
+			rotation.y = -3.141593
+		is_rotating = true
+		target_rotation = rotation.y + 0.785398
+		# Smoothly rotates the player along with the camera
+		while rotation.y <= target_rotation - 0.1:
+			print(rotation.y)
+			rotation.y -= (rotation.y - target_rotation) / 2
+			yield(get_tree().create_timer(0.05), "timeout")
+			print("rotated successfully")
+		# When close enough, snaps the rotation to the target rotation
+		rotation.y = target_rotation
+		print(rotation.y)
+		is_rotating = false
+		
+	elif rotate_direction == "right" && is_rotating == false:
+		if is_equal_approx(-3.141593, rotation.y) == true:
+			rotation.y = 3.141593
+		is_rotating = true
+		target_rotation = rotation.y - 0.785398
+		# Smoothly rotates the player along with the camera
+		while rotation.y >= target_rotation + 0.1:
+			rotation.y -= (rotation.y - target_rotation) / 2
+			yield(get_tree().create_timer(0.05), "timeout")
+			print("rotated successfully")
+			print(rotation.y)
+		# When close enough, snaps the rotation to the target rotation
+		rotation.y = target_rotation
+		print(rotation.y)
+		is_rotating = false
+
 
 func _physics_process(delta):
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = move_toward(0, direction.x, .95) * speed
 		velocity.z = move_toward(0, direction.z, .95) * speed
-	if direction.x == 0 && direction.z == 0:
-		if (velocity.x >= -0.01 && velocity.x <= 0.01) && (velocity.z >= -0.01 && velocity.z <= 0.01):
-			velocity.x = 0
-			velocity.z = 0
-		else:
-			velocity.x -= velocity.x / 4
-			velocity.z -= velocity.z / 4
+	elif direction == Vector3.ZERO && range(-0.01, 0.01).has(velocity.x) == false && range(-0.01, 0.01).has(velocity.x) == false:
+		velocity.x -= velocity.x / 4
+		velocity.z -= velocity.z / 4
+	elif direction == Vector3.ZERO:
+		velocity.x = 0
+		velocity.z = 0
 	
+	print(rotation.y)
+
 	velocity.y -= delta * GRAVITY
-	
 	velocity = move_and_slide(velocity, Vector3.UP, true)
-	
-	print(velocity.z)
-	
+
 
 func _input(_event):
+	if Input.is_action_just_pressed("camera_left"):
+		rotate_player("left")
+	elif Input.is_action_just_pressed("camera_right"):
+		rotate_player("right")
+
 	if Input.is_action_just_pressed("jump"):
 		velocity.y += JUMP_FORCE
-	if Input.is_action_pressed("camera_left"):
-		rotation.y += lerp_angle(rotation.y, rotation.y+1, 0)
