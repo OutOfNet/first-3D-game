fb96f241c17c99411797bdb3d6ce92de3f7aee7f
OutOfNet <neteoris@proton.me>
2025-05-10 12:35:08 +0200

    Camera rotation fixes.
    Fixed the camera getting offset if too many rotation inputs happen at the same time by adding a 20ms delay before rotating the camera and checking whether or not the camera angle is offset, and if it is, snap it back to the nearest normal angle (the function is an updated version of one made by avencherus on the godot forums a while back).

--- scripts/player/player_management.gd
+++ scripts/player/player_management.gd
@@ -4,34 +4,54 @@ const GRAVITY = 150
 const MAX_HEALTH = 150
 const JUMP_FORCE = 50
 const CAMERA_NEUTRAL_ROTATION = -0.35
+const INT_MAX = 2147483647
 
 export var speed = 15.0
 
-var target_rotation = 0
+var target_rotation = Vector2.ZERO
 var velocity = Vector3.ZERO
 var health = MAX_HEALTH
 var is_rotating = false
 var camera
+
+var camera_angles = [0, 0.785398, 1.570796, 2.356194, 3.141592, -3.141593, -2.356195, -1.570797, -0.785399, -0.000001]
 
 
 
 func _ready():
 	camera = get_node("Camera")
 
+
+func find_closest(value, array) -> float:
+	
+	var best_match = null
+	var least_diff = INT_MAX
+	
+	for number in array:
+		
+		var diff = abs(value - number)
+		
+		if(diff < least_diff):
+			best_match = number
+			least_diff = diff
+			
+	return best_match
+
+
 func rotate_camera(rotate_direction):
 	if rotate_direction == "left" && is_rotating == false:
 		if is_equal_approx(3.141593, rotation.y) == true:
 			rotation.y = -3.141593
 		is_rotating = true
-		target_rotation = rotation.y + 0.785398
+		target_rotation.x = rotation.y + 0.785398
 		# Smoothly rotates the player along with the camera
-		while rotation.y <= target_rotation - 0.1:
+		while rotation.y <= target_rotation.x - 0.02:
 			print(rotation.y)
-			rotation.y -= (rotation.y - target_rotation) / 2
-			yield(get_tree().create_timer(0.05), "timeout")
+			rotation.y -= (rotation.y - target_rotation.x) / 4
+			yield(get_tree().create_timer(0.0125), "timeout")
 			print("rotated successfully")
 		# When close enough, snaps the rotation to the target rotation
-		rotation.y = target_rotation
+		rotation.y = target_rotation.x
 		print(rotation.y)
 		is_rotating = false
 		
@@ -39,15 +59,15 @@ func rotate_camera(rotate_direction):
 		if is_equal_approx(-3.141593, rotation.y) == true:
 			rotation.y = 3.141593
 		is_rotating = true
-		target_rotation = rotation.y - 0.785398
+		target_rotation.x = rotation.y - 0.785398
 		# Smoothly rotates the player along with the camera
-		while rotation.y >= target_rotation + 0.1:
-			rotation.y -= (rotation.y - target_rotation) / 2
-			yield(get_tree().create_timer(0.05), "timeout")
+		while rotation.y >= target_rotation.x + 0.02:
+			rotation.y -= (rotation.y - target_rotation.x) / 4
+			yield(get_tree().create_timer(0.0125), "timeout")
 			print("rotated successfully")
 			print(rotation.y)
 		# When close enough, snaps the rotation to the target rotation
-		rotation.y = target_rotation
+		rotation.y = target_rotation.x
 		print(rotation.y)
 		is_rotating = false
 
@@ -65,6 +85,10 @@ func _physics_process(delta):
 	elif direction == Vector3.ZERO:
 		velocity.x = 0
 		velocity.z = 0
+	
+	if camera_angles.has(rotation.y) == false && is_rotating == false:
+		print(find_closest(rotation.y, camera_angles), " | ", rotation.y)
+		rotation.y = find_closest(rotation.y, camera_angles)
 
 	velocity.y -= delta * GRAVITY
 	velocity = move_and_slide(velocity, Vector3.UP, true)
@@ -72,35 +96,46 @@ func _physics_process(delta):
 
 func _input(_event):
 	if Input.is_action_just_pressed("camera_left"):
-		rotate_camera("left")
+		if is_rotating == false:
+			yield(get_tree().create_timer(0.02), "timeout")
+			rotate_camera("left")
 	elif Input.is_action_just_pressed("camera_right"):
-		rotate_camera("right")
-	
-	if Input.is_action_just_pressed("camera_up"):
-		target_rotation = camera.rotation.x + 0.775
-		# Smoothly rotates the camera upwards.
-		while camera.rotation.x <= target_rotation - 0.025:
+		if is_rotating == false:
+			yield(get_tree().create_timer(0.02), "timeout")
+			rotate_camera("right")
+	elif Input.is_action_just_pressed("camera_up"):
+		if is_rotating == false:
+			is_rotating = true
+			yield(get_tree().create_timer(0.02), "timeout")
+			target_rotation.y = camera.rotation.x + 0.5
+			# Smoothly rotates the camera upwards.
+			while camera.rotation.x <= target_rotation.y - 0.025:
+				print(camera.rotation.x)
+				camera.rotation.x -= (camera.rotation.x - target_rotation.y) / 4
+				yield(get_tree().create_timer(0.0125), "timeout")
+				print("rotated successfully")
+			camera.rotation.x = target_rotation.y
 			print(camera.rotation.x)
-			camera.rotation.x -= (camera.rotation.x - target_rotation) / 4
-			yield(get_tree().create_timer(0.025), "timeout")
-			print("rotated successfully")
-		camera.rotation.x = target_rotation
-		print(camera.rotation.x)
 	elif Input.is_action_just_pressed("camera_down"):
-		target_rotation = camera.rotation.x - 0.775
-		# Smoothly rotates the camera downwards.
-		while camera.rotation.x >= target_rotation + 0.025:
-			camera.rotation.x -= (camera.rotation.x - target_rotation) / 4
-			yield(get_tree().create_timer(0.025), "timeout")
-		camera.rotation.x = target_rotation
-		print("fully finished rotating")
-	elif Input.is_action_just_released("camera_down") or Input.is_action_just_released("camera_up"):
-		target_rotation = CAMERA_NEUTRAL_ROTATION
+		if is_rotating == false:
+			is_rotating = true
+			yield(get_tree().create_timer(0.02), "timeout")
+			target_rotation.y = camera.rotation.x - 0.5
+			# Smoothly rotates the camera downwards.
+			while camera.rotation.x >= target_rotation.y + 0.025:
+				camera.rotation.x -= (camera.rotation.x - target_rotation.y) / 4
+				yield(get_tree().create_timer(0.0125), "timeout")
+			camera.rotation.x = target_rotation.y
+			print("fully finished rotating")
+	if Input.is_action_just_released("camera_down") or Input.is_action_just_released("camera_up"):
+		target_rotation.y = CAMERA_NEUTRAL_ROTATION
+		yield(get_tree().create_timer(0.02), "timeout")
 		# Smoothly rotates the camera towards the neutral rotation.
-		while camera.rotation.x <= target_rotation + 0.025 && camera.rotation.x >= target_rotation - 0.025:
-			camera.rotation.x -= (camera.rotation.x - target_rotation) / 4
-			yield(get_tree().create_timer(0.025), "timeout")
-		camera.rotation.x = target_rotation
+		while camera.rotation.x >= target_rotation.y + 0.025 or camera.rotation.x <= target_rotation.y - 0.025:
+			camera.rotation.x -= (camera.rotation.x - target_rotation.y) / 4
+			yield(get_tree().create_timer(0.0125), "timeout")
+		camera.rotation.x = target_rotation.y
+		is_rotating = false
 		print(camera.rotation.x)
 
 	if Input.is_action_just_pressed("jump"):
